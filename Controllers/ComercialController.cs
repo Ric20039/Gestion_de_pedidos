2ce6c671685e902102aeb6d14e8b61e8bc1ef5ab
Ric20039 <20039ricardo@gmail.com>
2025-05-27 08:19:15 -0600

    hola

---
+++ Controllers/ComercialController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gestion_de_pedidos.Data;
+using Gestion_de_pedidos.Models;
+using System.Linq;
+
+namespace Gestion_de_pedidos.Controllers
+{
+    public class ComercialController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ComercialController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var comerciales = _context.Comercial.ToList(); // obtiene todos los comerciales
+            return View("~/Views/Comercial/Index.cshtml",comerciales);
+        }
+    }
+}

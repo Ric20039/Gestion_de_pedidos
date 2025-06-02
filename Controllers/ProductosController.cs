e00d7e5b1bac36a32774d388d7e224b33061b42a
Alejandro <Usuario@HPNOTE>
2025-06-01 20:35:15 -0600

    Productos

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestion_de_pedidos.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
